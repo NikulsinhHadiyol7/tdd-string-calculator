b0951a0bb6f32e351092c6592b1f88a97e4394e7
nikulsinh <nikulsinhrajput8@gmail.com>
2024-12-19 16:11:14 +0530

    feat: throw exception for negative numbers with details

--- src/calculator.js
+++ src/calculator.js
@@ -12,6 +12,11 @@ function add(numbers) {
         .split(new RegExp(delimiter))
         .map(Number);
 
+    const negatives = nums.filter((num) => num < 0);
+    if (negatives.length > 0) {
+        throw new Error(`Negative numbers not allowed: ${negatives.join(", ")}`);
+    }
+
     return nums.reduce((sum, num) => sum + num, 0);
 }
 
